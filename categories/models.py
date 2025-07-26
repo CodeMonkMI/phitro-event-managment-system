7037d9bbce7f89c840f1859ba66a070ac444561d
Monirul Islam <mr.monirul.dev@gmail.com>
2025-07-26 12:23:04 +0600

    update categories field

--- categories/models.py
+++ categories/models.py
@@ -5,7 +5,7 @@ from uuid import uuid4
 # Create your models here.
 class Category(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    name = models.CharField(max_length=10)
+    name = models.CharField(max_length=50)
     description = models.TextField()
 
     created_at = models.DateTimeField(auto_now_add=True)
